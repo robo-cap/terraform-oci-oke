205e50910c683b451c66c20516ba16028868bbcf
orasenatdpltintegration03/oracleidentitycloudservice/andrei.ilas@oracle.com <andrei.ilas@oracle.com>
2024-03-01 20:08:45 +0200

    fix compatibility with ORM

--- versions.tf
+++ versions.tf
@@ -1,8 +1,10 @@
 # Copyright (c) 2017, 2023 Oracle Corporation and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl
 
+
 terraform {
-  required_version = ">= 1.3.0"
+  experiments = [module_variable_optional_attrs]
+  required_version = ">= 1.2.9"
 
   required_providers {
     cloudinit = {
